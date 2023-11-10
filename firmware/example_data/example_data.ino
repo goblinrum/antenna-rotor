44bce106766628decf319b84ac81f3cdbbdb3747
goblinrum <ryan.ma3011@berkeley.edu>
2023-11-10 12:32:14 -0800

    GPS read example

--- firmware/example_data/example_data.ino
+++ firmware/example_data/example_data.ino
@@ -1,16 +1,31 @@
 // #include <WiFi.h>
 // #include <HTTPClient.h>
 
+unsigned char gps[64];
+int count = 0; 
+
 void setup() {
     // setup gps
+    Serial1.begin(9600); // Serial for the GPS module, will need to define TX and RX for ESP32
     // setup motors here
     // set up serial connection to computer here
-    Serial.begin(9600); // Starts the serial communication
+    Serial.begin(9600); // Starts the serial communication to the computer
 
 }
 
 void loop() {
     // Step 1: read the gps data
+    if (Serial1.available()) {
+      while (Serial1.available()) {
+        gps[count++] = Serial1.read();
+        if (count == 64) {
+          break;
+        }
+      }
+      Serial.write(gps, count);
+      clearBuffer();
+      count = 0;
+    }
     // Step 2: 
     //  - Option 1: use the gps data to directly request API
     //  - Option 2: send GPS data to computer, and have computer request API
@@ -24,4 +39,10 @@ void loop() {
         Serial.println("1,1,1,1"); // Send actual coordinates here
       }
     }
+}
+
+void clearBuffer() {
+  for (int i = 0; i < count; i++) {
+    gps[i] == NULL;
+  }
 }
