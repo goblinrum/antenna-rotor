7b29a4d4fd3c50882ef9bc3ef812aa97606d119d
Rickey McGregor <rickeymcgregor01@gmail.com>
2023-12-12 16:58:24 -0800

    forgot file

--- web/get_iss.js
+++ web/get_iss.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
-
-axios.get('http://wednesdays.ddns.net:9001/calculate_iss_position.ddns.net:9001/calculate_iss_position')
+/*axios.get('http://wednesdays.ddns.net:9001/calculate_iss_position')
     .then(response => {
         // Check if the request was successful (status code 200)
         if (!response.ok) {
@@ -22,22 +20,27 @@ axios.get('http://wednesdays.ddns.net:9001/calculate_iss_position.ddns.net:9001/
         // Handle errors during the fetch operation
         console.error('Fetch error:', error);
     });
-
+*/
 
 class ISS {
 
     constructor() {
+        this.azimuth = null;
         this.elevation = null;
         this.latitude = null;
         this.longitude = null;
         this.user_latitude = null
         this.user_longitude = null;
     }
-
+    
+    set_azimuth(data) {
+        this.azimuth = data.iss_position.azimuth;
+    }
+    
     set_elevation(data) {
         this.elevation = data.iss_position.elevation;
     }
-    
+
     set_latitude(data) {
         this.latitude = data.iss_position.latitude;
     }
@@ -51,7 +54,7 @@ class ISS {
     }
     
     set_user_longitude(data) {
-        this.user_longitude = data.user.location.longitude;
+        this.user_longitude = data.user_location.longitude;
     }
 
 }
@@ -60,23 +63,24 @@ class ISS {
 
 async function getISSData() {
     try {
-        const response = await get('http://wednesdays.ddns.net:9001/calculate_iss_position');
+        const response = await fetch('http://wednesdays.ddns.net:9001/calculate_iss_position');
         
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
   
         const data = await response.json();
-        console.log(response);
+        console.log(data);
 
         const iss = new ISS();
+        iss.set_azimuth(data)
         iss.set_elevation(data);
         iss.set_latitude(data);
         iss.set_longitude(data);
         iss.set_user_latitude(data);
         iss.set_user_longitude(data)
 
-        return [iss.elevation, iss.latitude, iss.longitude, iss.user_latitude, iss.user_longitude];
+        return [iss.azimuth, iss.elevation, iss.latitude, iss.longitude, iss.user_latitude, iss.user_longitude];
     } 
     
     catch (error) {
@@ -85,4 +89,3 @@ async function getISSData() {
     }
 }
 
-export { getISSData };
